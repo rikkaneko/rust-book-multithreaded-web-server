a8423bc37ca8e3ad91a7c1a441a13f98efa561d4
Joe Ma <rikkaneko23@gmail.com>
2021-12-16 23:46:34 +0800

    Update copyright.
    Signed-off-by: Joe Ma <rikkaneko23@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+/*
+ * This file is part of rust-book-multithreaded-web-server.
+ * Copyright (c) 2021-2021 Joe Ma <rikkaneko23@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use lazy_static::lazy_static;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
